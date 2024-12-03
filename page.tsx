887a68226808674b7b310a19172c43f9d014b681
saima <159808338+saima498@users.noreply.github.com>
2024-12-03 14:12:29 +0500

    hello-world

---
+++ page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+  <div className=" bg-orange-600  h-screen w-[1590px]">
+<h1 className="text-red-800 pt-80 font-bold text-[100px] text-center">
+ HELLO WORLD.
+</h1>
+  </div>  
+
+);
+}
